418b38ea364e4e8e12b345130d086c5de64d1dbc
Video Room <videoroom@Video-Room.local>
2015-10-14 10:21:25 -0700

    add timeout handling

--- client.rb
+++ client.rb
@@ -3,9 +3,17 @@
 require 'rest-client'
 url=ARGV[0]
 video=ARGV[1]
-print "Asking server at #{url} to encode #{video}\n"
+puts "Asking server at #{url} to encode #{video}\n"
 
-response = RestClient.post "#{url}/encode", :media => File.new(video, 'rb'), :size => "720x480", :container => "mp4", :vcodec => "libx264", :vbitrate => "512", :acodec => "aac", :abitrate => "128"
+response = RestClient::Resource.new("#{url}/encode", :read_timeout => 1800)
+
+begin
+  response.post  :media => File.new(video, 'rb'), :size => "720x480", :container => "mp4", :vcodec => "libx264", :vbitrate => "512", :acodec => "aac", :abitrate => "128"
+rescue Errno::EPIPE
+  puts "Connection broke!  Retrying..."
+  response.post  :media => File.new(video, 'rb'), :size => "720x480", :container => "mp4", :vcodec => "libx264", :vbitrate => "512", :acodec => "aac", :abitrate => "128"
+end
+
 File.open("media/#{video}", "w") do |f|
   f.write(response.body)
 end
